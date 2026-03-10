eced865be483ae1d5ecac91e149e5f7533b8818e
Sij-Coco <parkourweapon@gmail.com>
2026-03-10 12:59:50 -0800

    first commit

---
+++ app/src/main/java/com/example/bcr/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.bcr;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.bcr.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
